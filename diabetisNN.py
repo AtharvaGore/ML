86099ab6a251cfa9e194ce70abe56ca5f7c8c9e5
atharvagore <atharvammg@gmail.com>
2025-08-16 13:06:18 +0530

    big changes to neuron.py

--- diabetisNN.py
+++ diabetisNN.py
@@ -5,7 +5,7 @@ final2=[]
 with open('diabetes.csv','r') as f:
     f.readline()
     
-    for i in range(750):
+    for i in range(700):
         row = f.readline().split(',')
         final2.append([float(row[-1])])
         row = row[0:len(row)-1]
@@ -21,8 +21,8 @@ Y=np.array(final2)
 X_mean = np.mean(X,axis=0)
 X_std = np.std(X,axis=0)
 X = (X-X_mean)/X_std
-node1 = neuron.Neuron([[1],[1],[1],[1],[1],[1],[1],[1]],[[1],[1]])
-node2 = neuron.Neuron([[1],[1],[1],[1],[1],[1],[1],[1]],[[1],[1]])
+node1 = neuron.Neuron([[1],[1],[1],[1],[1],[1],[1],[1]],[1,-1])
+node2 = neuron.Neuron([[1],[1],[1],[1],[1],[1],[1],[1]],[0,0])
 
 node1input = np.dot(X,node1.inputs) + node1.bias
 node2input = np.dot(X,node2.inputs) + node2.bias
@@ -44,7 +44,7 @@ pN = eN/(eY + eN)
 
 temp = (1/(1+np.exp(Xtheta1)))*np.exp(Xtheta1)
 temp2 = []
-for i in range(750):
+for i in range(700):
     if Y[i,0] == 1:
         temp2.append([pN[i,0]*(theta5-theta3)])
     else:
@@ -56,7 +56,7 @@ temp2 = X*temp2
 
 
 summation=0
-for j in range(750):
+for j in range(10000):
 
     node1input = np.dot(X,node1.inputs) + node1.bias
     node2input = np.dot(X,node2.inputs) + node2.bias
@@ -77,21 +77,21 @@ for j in range(750):
 
     summation=0
     summation = np.mean(pY-Y)
-    bias3 = bias3 - 0.01*summation
+    bias3 = bias3 - 0.05*summation
     summation = np.mean(pN-(1-Y))
-    bias4 = bias4 - 0.01*summation
+    bias4 = bias4 - 0.05*summation
     summation = np.mean((pY-Y)*node1input)
-    theta3 = theta3 - 0.01*summation
+    theta3 = theta3 - 0.05*summation
     summation = np.mean((pY-Y)*node2input)
-    theta5 = theta5 - 0.01*summation
+    theta5 = theta5 - 0.05*summation
     summation = np.mean((pN-(1-Y))*node1input)
-    theta4 = theta4 - 0.01*summation
+    theta4 = theta4 - 0.05*summation
     summation = np.mean((pN-(1-Y))*node2input)
-    theta6 = theta6 - 0.01*summation
+    theta6 = theta6 - 0.05*summation
 
     temp = (1/(1+np.exp(Xtheta1)))*np.exp(Xtheta1)
     temp2 = []
-    for i in range(750):
+    for i in range(700):
         if Y[i,0] == 1:
             temp2.append([pN[i,0]*(theta5-theta3)])
         else:
@@ -108,7 +108,7 @@ for j in range(750):
 
     temp = (1/(1+np.exp(Xtheta2)))*np.exp(Xtheta2)
     temp2 = []
-    for i in range(750):
+    for i in range(700):
         if Y[i,0] == 1:
             temp2.append([pN[i,0]*(theta6-theta4)])
         else:
@@ -129,40 +129,52 @@ for j in range(750):
 #print(node1.inputs)
 
 with open('diabetes.csv','r') as f:
-    for i in range(750):
+    for i in range(709):
         f.readline()
-    
+    node3 = neuron.Neuron([[0.3], [-0.2], [0.1], [0.05], [-0.25], [0.15], [0.2], [-0.1]],[0.5, -0.5])
+    node4 = neuron.Neuron([[-0.15], [0.25], [0.2], [-0.05], [0.3], [-0.2], [0.1], [0.05]],[-0.4, 0.4])
+
+    obj=neuron.NeuralNetwork(X,Y,node3,node4)
+    obj.train()
+
     count=0
-    for i in range(10):
+    for i in range(50):
         row = f.readline().split(',')
         answer = float(row[-1])
         row = row[0:len(row)-1]
-        print(row)
+        #print(row)
         temp=[]
         for j in row:
             temp+=[float(j)]
         row = np.array(temp)
         row = (row-X_mean)/X_std
-        print(row)
-        node1input = np.dot(row,node1.inputs) + node1.bias
-        node2input = np.dot(row,node2.inputs) + node2.bias
-        Xtheta1 = node1input
-        Xtheta2 = node2input
 
-        node1input = np.log1p(np.exp(node1input))
-        node2input = np.log1p(np.exp(node2input))
-
-        rawY = node1input*theta3 + node2input*theta4 + bias3
-        rawN = node1input*theta5 + node2input*theta6 + bias4
-        print(rawY,rawN)
-        print(answer)
-        if rawY>rawN:
+        prediction = obj.forwardPass(row)[0]
+        if prediction[0]>prediction[1]:
             if answer == 1:
                 count+=1
-        elif rawN>rawY:
+        elif prediction[1]>prediction[0]:
             if answer == 0:
                 count+=1
-    print(count/10)
+    print(count/50)
+
+    # node1input = np.dot(row,node1.inputs) + node1.bias
+    # node2input = np.dot(row,node2.inputs) + node2.bias
+    # Xtheta1 = node1input
+    # Xtheta2 = node2input
+
+    # node1input = np.log1p(np.exp(node1input))
+    # node2input = np.log1p(np.exp(node2input))
+
+    # rawY = node1input*theta3 + node2input*theta4 + bias3
+    # rawN = node1input*theta5 + node2input*theta6 + bias4
+    # eY = np.exp(rawY)
+    # eN = np.exp(rawN)
+
+    # pY = eY/(eY + eN)
+    # pN = eN/(eY + eN)
+    # print(pY)
+
 
 
 
--- neuron.py
+++ neuron.py
@@ -4,3 +4,70 @@ class Neuron:
         self.inputs = np.array(inputs)
         self.outputs=np.array(outputs)
         self.bias=0.0
+
+
+
+class NeuralNetwork:
+    def __init__(self,data,expected,*nodes):
+        self.data = data
+        self.Y = expected
+        self.hiddenNodes = nodes
+        self.outputBias = [0.0,0.0]
+        
+    def softplus(nodeinput):
+        return np.log1p(np.exp(nodeinput))
+
+    def forwardPass(self,inputs):
+        nodeinputs = []
+        for i in range(len(self.hiddenNodes)):
+            nodeinputs.append(np.dot(inputs,self.hiddenNodes[i].inputs) + self.hiddenNodes[i].bias)
+        beforeSoftPlus = nodeinputs
+        # print(beforeSoftPlus[0])
+        nodeinputs = [NeuralNetwork.softplus(i) for i in nodeinputs]
+        rawOutput = [self.outputBias[0],self.outputBias[1]]
+        # print(rawOutput)
+        for i in range(2):
+            for j in range(len(nodeinputs)):
+               rawOutput[i] += nodeinputs[j]*self.hiddenNodes[j].outputs[i]
+
+        expRawOutput = [np.exp(i) for i in rawOutput]
+        pY = expRawOutput[0]/(expRawOutput[0]+expRawOutput[1])
+        pN = expRawOutput[1]/(expRawOutput[0]+expRawOutput[1])
+        # print(pY)
+        return ([pY,pN],nodeinputs,beforeSoftPlus,rawOutput)
+    
+    
+    
+    def train(self):
+        rate = 0.01
+        for i in range(10000):
+            predictedProbabilities = self.forwardPass(self.data)
+            summation=0
+            self.outputBias[0] = self.outputBias[0] - rate*np.mean(predictedProbabilities[0][0]-self.Y)
+            self.outputBias[1] = self.outputBias[1] - rate*np.mean(predictedProbabilities[0][1]-(1-self.Y))
+            for i in range(2):
+                self.hiddenNodes[i].outputs[0] -=rate*np.mean((predictedProbabilities[0][0]-self.Y)*predictedProbabilities[1][i])
+                self.hiddenNodes[i].outputs[1] -=rate*np.mean((predictedProbabilities[0][1]-(1-self.Y))*predictedProbabilities[1][i])
+            
+            for j in range(2):
+                temp = (1/(1+np.exp(-predictedProbabilities[2][j])))
+                temp2 = []
+                for i in range(700):
+                    if self.Y[i,0] == 1:
+                        temp2.append([predictedProbabilities[0][1][i,0]*(self.hiddenNodes[j].outputs[1]-self.hiddenNodes[j].outputs[0])])
+                    else:
+                        temp2.append([predictedProbabilities[0][0][i,0]*(self.hiddenNodes[j].outputs[0]-self.hiddenNodes[j].outputs[1])])
+                temp2 = np.array(temp2)
+                temp2 = temp*temp2
+                summation = temp2.mean()
+                #print(summation)
+                self.hiddenNodes[j].bias = self.hiddenNodes[j].bias - rate*summation
+                temp2 = self.data*temp2
+
+                for i in range(8):
+                    summation = temp2[:, i].mean()
+                    self.hiddenNodes[j].inputs[i,0] = self.hiddenNodes[j].inputs[i,0] - rate*summation
+                summation=0
+                
+        
+        
